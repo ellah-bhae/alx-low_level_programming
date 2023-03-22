c77a428bb99de05825cfa0748a290fc83e5de8ef
ellah-bhae <emmanuella.afeke@gmail.com>
2023-03-22 13:52:17 -0700

    i figured it out

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print multiple times the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i <= 9)
+	for (c = 'a'; c <= 'z'; c++)
+	{
+
+	_putchar(c);
+
+	_putchar('\n');
+	c++;
+	}
+}
