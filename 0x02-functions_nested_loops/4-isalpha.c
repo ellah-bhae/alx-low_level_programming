49bce4db35410bdb43efcb60cf70f1c91fae98c6
ellah-bhae <emmanuella.afeke@gmail.com>
2023-03-23 05:17:06 -0700

    pritnting alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * _islower(int c) - function to print alphabets
- *@c: The parameter to be printed
-*
+ * _isalpha - function to print alphabets
+ * @c: The parameter to be printed
+ *
  * Return: 1 if it is an alphabet
  * and 0 otherwise
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-        if ((c >= 97 && c <= 122 ||  (c>= 65 && c<= 90))
-			{	
-return (1);
-}
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 
-        else
-	{
-return (0); }
+		return (1);
+
+
+
+	else
+
+		return (0);
 }
